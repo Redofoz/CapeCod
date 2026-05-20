36e1595159b709598b31aecb6b5c7c9b0165f0fb
Redofoz <Redofoz@aol.com>
2026-05-20 11:53:06 -0600

    Update server URL in script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@
   const emailInput = document.getElementById("emailInput");
   const toggleEye = document.getElementById("toggleEye");
 
-  const serverUrl = "https://a979-2600-1011-b367-706a-1830-e50-60c1-158a.ngrok-free.app";
+  const serverUrl = "https://efff-142-190-94-2.ngrok-free.app";
 
   // Utilities
   function getTimestamp() {
